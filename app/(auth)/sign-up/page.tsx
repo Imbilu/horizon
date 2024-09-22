a5fd20dbe47941a8f75842e0d062e20b906ca9de
Imbilu <imbilusydney09@gmail.com>
2024-09-22 14:33:36 +0300

    suppress typescript build errors

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,7 @@
 import AuthForm from "@/components/AuthForm";
-import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
 async function SignUp() {
-    const loggedInUser = await getLoggedInUser();
     return (
         <section className="flex-center size-full max-sm:px-6">
             <AuthForm type="sign-up" />
