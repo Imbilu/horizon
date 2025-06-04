b8f2b74aa253ec2b60b28ea7eca931ee826725d6
Imbilu <imbilusydney09@gmail.com>
2025-06-04 18:14:10 +0300

    fix user sign-in issue

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -6,7 +6,7 @@ import React from "react";
 
 async function MyBanks() {
     const loggedIn = await getLoggedInUser();
-    const accounts = await getAccounts({ userId: loggedIn?.$id });
+    const accounts = await getAccounts({ userId: loggedIn?.userId });
 
     return (
         <section className="flex">
